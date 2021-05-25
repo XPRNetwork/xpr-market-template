1c90cd234576d46b752b9b41470753e005c17074
Cindy Sun <c.s.sun5@gmail.com>
2021-05-25 12:00:34 -0700

    Add reset css and breakpoints

---
+++ next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  images: {
+    domains: [
+      'cloudflare-ipfs.com',
+      'gateway.pinata.cloud',
+      'ipfs.io',
+      'bloks.io',
+    ],
+  },
+  target: 'serverless',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack', 'url-loader'],
+    });
+
+    return config;
+  },
+};
