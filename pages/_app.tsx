7897075b54287a49f6d62faa5b749eabf9677da2
Cindy Sun <c.s.sun5@gmail.com>
2021-05-25 12:58:49 -0700

    Clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import '../styles/globals.css'
 import { ThemeProvider } from "styled-components";
 import theme from '../styles/theme';
+import '../styles/reset.css';
+import '../styles/globals.css';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
-
     </ThemeProvider>
   );
 }
