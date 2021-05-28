453d1eda3594de2fafc6d6c0231c1c0d885a4f22
Andrew Park <andrewpark9517@gmail.com>
2021-05-28 14:36:36 -0700

    style update

--- components/Header/Header.styled.ts
+++ components/Header/Header.styled.ts
@@ -1,50 +1,133 @@
 import styled from 'styled-components';
 import customizationJson from '../../custom/customization';
+import { MaxWidth } from '../../styles/MaxWidth.styled';
+import { breakpoint } from '../../styles/Breakpoints';
 
 const { typography, header } = customizationJson;
-const { highlightFont } = header;
+const {
+  imagePlacement,
+  backgroundColor,
+  highlightFont,
+  mainHeadingFont,
+  subheadingFont,
+  button,
+} = header;
 
 export const HeaderContainer = styled.div`
-  background-color: ${({ theme }) => theme.header.backgroundColor || 'blue'};
+  background-color: ${backgroundColor || 'blue'};
+
+  ${breakpoint.tablet`
+    margin-top: 30px;
+  `}
+`;
+
+export const HeaderContent = styled(MaxWidth)`
   display: flex;
-  flex-direction: ${({ theme }) =>
-    theme.header.imagePlacement === 'right' ? 'row' : 'row-reverse'};
+  flex-direction: ${imagePlacement === 'right' ? 'row' : 'row-reverse'};
   height: 640px;
+
+  ${breakpoint.tablet`
+    flex-direction: column;
+    height: 100%;
+  `}
 `;
 
 export const TextContainer = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: ${header.imagePlacement === 'right' ? 'flex-start' : 'flex-end'};
+  text-align: ${header.imagePlacement === 'right' ? 'left' : 'right'};
+  line-height: 1.78;
+
+  ${breakpoint.tablet`
+    text-align: left;
+    align-items: flex-start;
+  `}
 `;
 
 export const ImageContainer = styled.div`
   flex: 1;
-  background-image: url('/header-image.png');
-  background-repeat: no-repeat;
-  background-size: cover;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  ${breakpoint.tablet`
+    margin-top: 30px;
+  `}
 `;
 
 export const HighlightText = styled.h3`
-  display: ${({ theme }) =>
-    theme.header.highlightFont.isShown ? 'block' : 'none'};
-  color: ${({ theme }) => theme.header.highlightFont.color};
-  font-family: ${({ theme }) => {
-    const chosenFont = theme.header.highlightFont.type;
-    return typography[chosenFont].font;
-  }};
-  font-size: ${({ theme }) => {
-    const chosenFont = theme.header.highlightFont.type;
-    return typography[chosenFont].size;
-  }};
-  font-weight: ${({ theme }) => {
-    const chosenFont = theme.header.highlightFont.type;
-    return typography[chosenFont].fontWeight;
-  }};
+  display: ${highlightFont.isShown ? 'block' : 'none'};
+  color: ${highlightFont.color};
+  font-family: ${typography[highlightFont.type].font};
+  font-size: ${typography[highlightFont.type].size};
+  font-weight: ${typography[highlightFont.type].fontWeight};
+  margin-bottom: 8px;
+
+  ${breakpoint.mobile`
+    font-size: 12px;
+  `}
 `;
 
-export const HeaderText = styled.h1``;
+export const HeaderText = styled.h1`
+  color: ${mainHeadingFont.color};
+  font-family: ${typography[mainHeadingFont.type].font};
+  font-size: ${typography[mainHeadingFont.type].size};
+  font-weight: ${typography[mainHeadingFont.type].fontWeight};
+  margin: 0;
+  line-height: 1.25;
 
-export const SubHeaderText = styled.h2``;
+  ${breakpoint.tablet`
+    font-size: 55px;
+  `}
 
-export const Button = styled.button``;
+  ${breakpoint.mobile`
+    font-size: 32px;
+  `}
+`;
 
-export const ButtonText = styled.span``;
+export const SubHeaderText = styled.h2`
+  color: ${subheadingFont.color};
+  font-family: ${typography[subheadingFont.type].font};
+  font-size: ${typography[subheadingFont.type].size};
+  font-weight: ${typography[subheadingFont.type].fontWeight};
+  margin-top: 16px;
+  max-width: 550px;
+
+  ${breakpoint.mobile`
+    font-size: 14px;
+    margin-top: 23px;
+  `}
+`;
+
+export const ButtonWrapperAnchor = styled.a.attrs({
+  href: button.link,
+  target: '_blank',
+  rel: 'noreferrer',
+})`
+  ${breakpoint.mobile`
+    width: 100%;
+  `}
+`;
+
+export const Button = styled.button`
+  width: 208px;
+  height: 56px;
+  margin-top: 32px;
+  display: ${button.isShown ? 'block' : 'none'};
+  background-color: ${button.backgroundColor};
+  font-family: ${typography[button.textFont].font};
+  color: ${button.textColor};
+  border: none;
+  cursor: pointer;
+
+  ${breakpoint.mobile`
+    width: 100%;
+    height: 32px;
+    margin-top: 25px;
+  `}
+`;
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,31 +1,42 @@
 import { FC } from 'react';
 import {
   HeaderContainer,
+  HeaderContent,
   TextContainer,
   ImageContainer,
   HighlightText,
   SubHeaderText,
   HeaderText,
+  ButtonWrapperAnchor,
   Button,
-  ButtonText,
 } from './Header.styled';
+import customizationJson from '../../custom/customization';
 
 export const Header: FC = () => {
+  const {
+    header: { image },
+  } = customizationJson;
+
   return (
     <HeaderContainer>
-      <TextContainer>
-        <HighlightText>Exclusive</HighlightText>
-        <HeaderText>Kenny Lofton </HeaderText>
-        <SubHeaderText>
-          Kenneth Lofton is an American former Major League Baseball center
-          fielder. Lofton was a six-time All-Star, four-time Gold Glove Award
-          winner, and at retirement.
-        </SubHeaderText>
-        <Button>
-          <ButtonText>Label</ButtonText>
-        </Button>
-      </TextContainer>
-      <ImageContainer />
+      <HeaderContent>
+        <TextContainer>
+          <HighlightText>Exclusive</HighlightText>
+          <HeaderText>Kenny Lofton </HeaderText>
+          <HeaderText>Baseball Card NFTs</HeaderText>
+          <SubHeaderText>
+            Kenneth Lofton is an American former Major League Baseball center
+            fielder. Lofton was a six-time All-Star, four-time Gold Glove Award
+            winner, and at retirement.
+          </SubHeaderText>
+          <ButtonWrapperAnchor>
+            <Button>Label</Button>
+          </ButtonWrapperAnchor>
+        </TextContainer>
+        <ImageContainer>
+          <img src={image} alt="mlb" />
+        </ImageContainer>
+      </HeaderContent>
     </HeaderContainer>
   );
 };
