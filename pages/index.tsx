8184feacc643ad6bf6a6c3c253157ac128f7b104
Andrew Park <andrewpark9517@gmail.com>
2021-06-01 16:52:06 -0700

    all functionality for template card data complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,29 +4,41 @@ import {
   getTemplatesByCollection,
   Template,
   getLowestPricesForAllCollectionTemplates,
+  getAllTemplatesForUserWithAssetCount,
 } from '../services/templates';
 import { FC } from 'react';
-import { Card, LoadingPage } from '../components';
+import { Card, LoadingPage, Header } from '../components';
+import { useLocaleContext } from '../components/Provider';
 
 const HomePage: FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [templates, setTemplates] = useState<Template[]>([]);
+  const { isLoadingLocale } = useLocaleContext();
 
   useEffect(() => {
     (async () => {
       setIsLoading(true);
-      const templates = await getTemplatesByCollection({ type: 'monsters' });
+      const templates = await getTemplatesByCollection({ type: 'alprclk' });
       setTemplates(templates);
       setIsLoading(false);
 
+      console.log('templates: ', templates);
+
       const prices = await getLowestPricesForAllCollectionTemplates({
-        type: 'monsters',
+        type: 'alprclk',
+        owner: 'alprclk',
       });
       console.log('prices: ', prices);
+
+      const onSale = await getAllTemplatesForUserWithAssetCount({
+        owner: 'alprclk',
+        collection: 'alprclk',
+      });
+      console.log('onsale: ', onSale);
     })();
   }, []);
 
-  if (isLoading) {
+  if (isLoading || isLoadingLocale) {
     return <LoadingPage />;
   }
 
