03a1a25e90b17ea889334ece99a4b21611e81bcd
Andrew Park <andrewpark9517@gmail.com>
2021-06-02 13:32:47 -0700

    added shimmer block

--- components/Card/Card.styled.ts
+++ components/Card/Card.styled.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import customizationJson from '../../custom/customization';
 
 const { nftCard } = customizationJson;
@@ -21,4 +21,21 @@ export const AssetContainer = styled.div`
   height: 0px;
   padding-bottom: 90%;
   background-color: gray;
+`;
+
+const placeHolderShimmer = keyframes`
+  \ 0% {
+    background-position: -500px 0
+  }
+  \ 100% {
+    background-position: 500px 0
+  }
+`;
+
+export const ShimmerBlock = styled.div`
+  animation: ${placeHolderShimmer} 1s linear infinite;
+  background: linear-gradient(to right, #eeeeee 8%, #e7e7e7 18%, #eeeeee 33%);
+  background-size: 1000px 18px;
+  width: 200px;
+  height: 14px;
 `;
