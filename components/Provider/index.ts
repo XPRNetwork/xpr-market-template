d2f51e129186d12446963288701915243026f23e
Joanna Chen <joanna@metalpay.co>
2021-06-03 14:28:40 -0700

    Revert unnecessary changes

--- components/Provider/index.ts
+++ components/Provider/index.ts
@@ -1,4 +1,2 @@
 export { LocaleProvider, useLocaleContext } from './LocaleProvider';
 export { useAuthContext, AuthProvider } from './AuthProvider';
-export { useModalContext, ModalProvider, MODAL_TYPES } from './ModalProvider';
-export type { CreateSaleModalProps } from './ModalProvider';
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,16 @@ import '../styles/reset.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Footer, Navbar } from '../components';
-import {
-  AuthProvider,
-  LocaleProvider,
-  ModalProvider,
-} from '../components/Provider';
+import { AuthProvider, LocaleProvider } from '../components/Provider';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <AuthProvider>
-      <ModalProvider>
-        <LocaleProvider>
-          <Navbar />
-          <Component {...pageProps} />
-          <Footer />
-        </LocaleProvider>
-      </ModalProvider>
+      <LocaleProvider>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </LocaleProvider>
     </AuthProvider>
   );
 }
