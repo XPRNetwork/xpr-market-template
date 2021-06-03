528ebf6ee6c6f370923d7eb1791105ce1fe682f5
Andrew Park <andrewpark9517@gmail.com>
2021-06-03 16:17:36 -0700

    my items page in prgoress

---
+++ components/MyItemsPage/MyItemsPage.styled.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { MaxWidth } from '../../styles/MaxWidth.styled';
+import customizationJson from '../../custom/customization';
+import { breakpoint } from '../../styles/Breakpoints';
+
+const {
+  myItemsPage: { backgroundColor, headerFont },
+  typography,
+} = customizationJson;
+
+export const PageContainer = styled(MaxWidth)`
+  background-color: ${backgroundColor};
+  flex-direction: column;
+  justify-content: flex-start;
+  min-height: calc(100vh - 96px - 96px);
+  overflow: scroll;
+
+  ${breakpoint.mobile`
+    ${`min-height: calc(100vh - 96px - 72px)`};
+  `}
+`;
+
+export const HeaderText = styled.span`
+  font-family: ${typography[headerFont.type].font};
+  font-size: ${typography[headerFont.type].size};
+  color: ${headerFont.color};
+  align-self: flex-start;
+  margin-top: 56px;
+  margin-bottom: 65px;
+  line-height: 1.25;
+`;
---
+++ pages/my-items.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+import { MaxWidth } from '../styles/MaxWidth.styled';
+import {
+  PageContainer,
+  HeaderText,
+} from '../components/MyItemsPage/MyItemsPage.styled';
+const MyItemsPage: FC = () => {
+  return (
+    <PageContainer>
+      <HeaderText>Hi</HeaderText>
+    </PageContainer>
+  );
+};
+
+export default MyItemsPage;
