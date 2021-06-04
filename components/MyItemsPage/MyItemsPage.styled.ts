23f4a57ed33b109d5724a86f8e343c21c3167505
Andrew Park <andrewpark9517@gmail.com>
2021-06-04 15:48:35 -0700

    mobile style

--- components/MyItemsPage/MyItemsPage.styled.ts
+++ components/MyItemsPage/MyItemsPage.styled.ts
@@ -28,4 +28,10 @@ export const HeaderText = styled.span`
   margin-top: 56px;
   margin-bottom: 65px;
   line-height: 1.25;
+
+  ${breakpoint.mobile`
+    margin-top: 31px;
+    margin-bottom: 40px;
+    font-size: 25px;
+  `}
 `;
