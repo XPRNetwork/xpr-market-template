e954137470d2b89d850e6e8006487e394c6af115
Andrew Park <andrewpark9517@gmail.com>
2021-06-08 10:44:46 -0700

    fix error in loading page

--- components/LoadingPage/LoadingPage.tsx
+++ components/LoadingPage/LoadingPage.tsx
@@ -1,13 +1,9 @@
 import { Container } from './LoadingPage.styled';
 import { Spinner } from '../Spinner/Spinner';
 
-type Props = {
-  margin: string;
-};
-
-export const LoadingPage = ({ margin }: Props): JSX.Element => {
+export const LoadingPage = (): JSX.Element => {
   return (
-    <Container margin={margin}>
+    <Container>
       <Spinner />
     </Container>
   );
