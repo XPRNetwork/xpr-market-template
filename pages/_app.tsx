6fc91efc1003931cc729e70f4998d1623be3044f
Joanna Chen <joanna@metalpay.co>
2021-06-15 18:55:27 -0700

    Clean up FeaturedSection storybook component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import { useState, useEffect } from 'react';
-import '../styles/globals.css';
 import '../styles/reset.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
